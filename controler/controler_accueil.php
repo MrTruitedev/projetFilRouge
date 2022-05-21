080d5fa19adad5a1fe91f2a5450eacfba3605fe9
MrTruite <tomtutti@hotmail.fr>
2022-05-21 16:39:16 +0200

    Update accueil controler

--- controler/controler_accueil.php
+++ controler/controler_accueil.php
@@ -2,9 +2,4 @@
     include './utils/connect_bdd.php';
     include './model/model_user.php';
     include './view/view_accueil.php';
-    if($_SESSION['fam'] == 1){
-        echo '<h1>MA SUPER PAGE D\'ACCUEIL D\'ARTISTE</h1>';
-    }else{
-        echo '<h1>MA SUPER PAGE D\'ACCUEIL D\'ORGANISATEUR</h1>';
-    }
 ?>
