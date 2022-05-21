6c4965f38c9851e4e3ffb3da745827312b5b48b2
MrTruite <tomtutti@hotmail.fr>
2022-05-21 16:41:10 +0200

    View accueil WIP
    Update view accueil with first version of project. WIP

--- view/view_accueil.php
+++ view/view_accueil.php
@@ -1,11 +1,66 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<div id="home-container">
+    <h1>Projet Fil Rouge</h1>
+    <hr />
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam accusamus eaque quidem voluptatem qui quae,
+        similique consequatur
+        est quod fuga nemo deleniti rerum odio nihil impedit nam vel perferendis ratione tempore architecto iste? Sunt
+        et tempore nesciunt itaque
+        fugit natus vitae quis earum aliquid, sequi similique cum qui adipisci veritatis repellat minus hic corporis
+        excepturi deleniti error quos!
+        oluptas dignissimos numquam veritatis, earum atque ducimus totam rerum culpa ipsam in vel quos architecto,
+        officia reiciendis quia porro cumque
+    </p>
+    <div id="home-grid1">
+        <div class="col1">
+            <img src="./asset/img/home1.jpg" alt="" class="home-img">
+        </div>
+        <div class="col2">
+            <h2>Trouvez rapidement des concerts près de chez vous</h2>
+            <p class="p-grid">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem ipsum aut, sequi praesentium quasi neque,
+                odio dignissimos animi laudantium
+                illum accusamus voluptatum, a non fugit? Modi, natus eveniet a cumque reprehenderit beatae possimus
+                cupiditate reiciendis iste qui? Omnis alias
+                iusto excepturi quaerat vitae exercitationem illum magnam ratione dolorum temporibus. Vel aspernatur
+                ullam quaerat pariatur. Tenetur quibusdam
+                qui minus praesentium quaerat ducimus optio error excepturi unde nam, veritatis omnis voluptates porro
+                aspernatur expedita dolores, cumque alias
+                similique natus pariatur. Quas, sint? Assumenda beatae nobis dicta nisi doloremque, cum repudiandae
+                laudantium aliquid dignissimos nulla! Iusto
+                praesentium sunt, repellendus sed aut aperiam minus alias soluta deleniti eaque vitae, harum debitis
+                quisquam dolores incidunt magnam explicabo
+                nihil? Neque dolore voluptatum fuga reprehenderit tempora dicta. Consectetur unde nobis consequatur
+                omnis quae reiciendis voluptatem incidunt molestiae
+                quasi dolorem eaque natus totam earum odio similique, accusantium deleniti ullam sunt magnam ducimus ab?
+                Unde illo quibusdam sunt aut?
+            </p>
+        </div>
+    </div>
+    <div id="home-grid2">
+        <div class="col2">
+            <img src="./asset/img/home2.jpg" alt="" class="home-img">
+        </div>
+        <div class="col1">
+            <h2>Profitez de notre réseau d'organisateurs certifiés !</h2>
+
+            <p class="p-grid">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque rerum odit atque rem minus? Est ipsa ex
+                eius. Perferendis
+                cupiditate expedita eaque cum beatae, fuga illo quasi ex doloribus, rem nobis accusantium eos illum. Cum
+                consectetur blanditiis dicta inventore
+                quis impedit iure autem tenetur? Quas dolorum cumque veniam necessitatibus facere animi iure velit sunt
+                ad? Voluptatum expedita exercitationem
+                eius quibusdam ad fugiat ex deserunt, et consectetur delectus eligendi ipsum cumque labore, beatae
+                facilis accusantium repellat doloremque ea iure
+                quis rerum earum. Facilis odit recusandae mollitia dolores quam in, numquam porro eaque repudiandae
+                laudantium fugit expedita asperiores. Suscipit
+                labore doloribus asperiores soluta. Nisi officiis et exercitationem deserunt maxime quasi temporibus,
+                libero, fuga, perferendis velit sunt omnis ipsa
+                expedita beatae! Aperiam repudiandae qui minima maxime sit vero fugiat et, laborum unde suscipit!
+                Aliquid eligendi impedit eveniet aspernatur eum fuga
+                expedita quasi quia assumenda, quae totam eaque deleniti quas iure unde ducimus voluptates?
+            </p>
+        </div>
+    </div>
+</div>
